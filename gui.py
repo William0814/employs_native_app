6e553ecc5f4d0ca350e66b435aeedbc4b722339c
william0814 <king-master90@hotmail.com>
2025-02-17 22:12:39 +0100

    Implement the add button

--- gui.py
+++ gui.py
@@ -2,10 +2,27 @@ import functions
 import FreeSimpleGUI as gui
 
 
-label = gui.Text("Action")
-input_box = gui.InputText(tooltip="Enter Employ")
+label = gui.Text("Action type: ")
+input_box = gui.InputText(tooltip="Enter Employ", key="employ")
 add_button = gui.Button("Add")
 
-window = gui.Window("My Employ App", layout=[[label], [input_box, add_button]])
-window.read()
+window = gui.Window("My Employ App",
+                    layout=[[label, input_box, add_button]],
+                    font= ("Helvetica", 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            employs = functions.get_employs()
+            new_employ = values['employ'].strip() + "\n"
+            employs.append(new_employ.title())
+            functions.write_employs(employs)
+
+        case gui.WIN_CLOSED:
+            break
+
+    
 window.close()
