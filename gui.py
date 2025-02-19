22fbcbc47a839adc7fe38f6490203f11c896f445
william0814 <king-master90@hotmail.com>
2025-02-19 21:54:40 +0100

    Create Edit button and actually the list

--- gui.py
+++ gui.py
@@ -5,21 +5,39 @@ import FreeSimpleGUI as gui
 label = gui.Text("Action type: ")
 input_box = gui.InputText(tooltip="Enter Employ", key="employ")
 add_button = gui.Button("Add")
+list_box = gui.Listbox(values=functions.get_employs(), key='employs', enable_events=True, size=[45, 10])
+
+edit_button = gui.Button("Edit")
 
 window = gui.Window("My Employ App",
-                    layout=[[label, input_box, add_button]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
                     font= ("Helvetica", 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['employs'])
     match event:
         case 'Add':
             employs = functions.get_employs()
             new_employ = values['employ'].strip() + "\n"
             employs.append(new_employ.title())
+            functions.write_employs(employs) 
+            window['employs'].update(values=employs)
+
+        case 'Edit':
+            employ_to_edit = values['employs'][0]
+            new_employ = values['employ']
+
+            employs = functions.get_employs()
+            index = employs.index(employ_to_edit)
+            employs[index] = new_employ.title()
             functions.write_employs(employs)
+            window['employs'].update(values=employs) 
+
+        case 'employs':
+            window['employ'].update(value=values['employs'][0])
 
         case gui.WIN_CLOSED:
             break
